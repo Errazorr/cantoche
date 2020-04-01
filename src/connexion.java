09a4d73fe451a591d9c2783e88aedd6393eb5fb2
Errazorr <56584432+Errazorr@users.noreply.github.com>
2020-04-01 12:31:24 +0200

    Chemin pour ouvrir la page de choix depuis la page de connexion

--- src/connexion.java
+++ src/connexion.java
@@ -62,7 +62,7 @@ public class connexion {
 			             ResultSet rs = stm.executeQuery("select * from compte where identifiant ='"+textId.getText()+"' and mdp ='"+textMdp.getText()+"'");
 
 			             if(rs.next()){
-			                 Accueil fen2 = new Accueil();
+			                 Accueil_test fen2 = new Accueil_test();
 			                 fenetre_co.close();
 			                 fen2.open();
 			             }
