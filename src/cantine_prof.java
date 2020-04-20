eee19377ba8fb81ce1efd9d3234e0cb854e41c43
Errazorr <56584432+Errazorr@users.noreply.github.com>
2020-04-20 10:27:35 +0200

    Modification du design et suppression d'un bouton

--- src/cantine_prof.java
+++ src/cantine_prof.java
@@ -50,15 +50,15 @@ public class cantine_prof {
 		
 		Button btnNewButton = new Button(shlCantineprof, SWT.NONE);
 		btnNewButton.setBounds(50, 576, 204, 35);
-		btnNewButton.setText("Ajouter un \u00E9tudiant");
+		btnNewButton.setText("Ajouter un professeur");
 		
 		Button btnNewButton_1 = new Button(shlCantineprof, SWT.NONE);
 		btnNewButton_1.setBounds(470, 576, 204, 35);
-		btnNewButton_1.setText("Modifier un \u00E9tudiant");
+		btnNewButton_1.setText("Modifier un professeur");
 		
 		Button btnNewButton_2 = new Button(shlCantineprof, SWT.NONE);
 		btnNewButton_2.setBounds(260, 576, 204, 35);
-		btnNewButton_2.setText("Supprimer un \u00E9tudiant");
+		btnNewButton_2.setText("Supprimer un professeur");
 		
 		table = new Table(shlCantineprof, SWT.BORDER | SWT.FULL_SELECTION);
 		table.setLinesVisible(true);
@@ -66,19 +66,15 @@ public class cantine_prof {
 		table.setBounds(50, 100, 835, 440);
 		
 		TableColumn tblclmnNom = new TableColumn(table, SWT.NONE);
-		tblclmnNom.setWidth(100);
+		tblclmnNom.setWidth(135);
 		tblclmnNom.setText("Nom");
 		
 		TableColumn tblclmnPrnom = new TableColumn(table, SWT.NONE);
-		tblclmnPrnom.setWidth(100);
+		tblclmnPrnom.setWidth(135);
 		tblclmnPrnom.setText("Pr\u00E9nom");
 		
-		TableColumn tblclmnClasse = new TableColumn(table, SWT.NONE);
-		tblclmnClasse.setWidth(100);
-		tblclmnClasse.setText("Classe");
-		
 		TableColumn tblclmnCantine = new TableColumn(table, SWT.NONE);
-		tblclmnCantine.setWidth(82);
+		tblclmnCantine.setWidth(85);
 		tblclmnCantine.setText("Cantine");
 		
 		TableColumn tblclmnJour = new TableColumn(table, SWT.NONE);
@@ -87,13 +83,14 @@ public class cantine_prof {
 		
 		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
 		tblclmnNewColumn.setText("R\u00E9gime");
-		tblclmnNewColumn.setWidth(166);
+		tblclmnNewColumn.setWidth(190);
 		
 		TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
 		tblclmnNewColumn_1.setWidth(182);
 		tblclmnNewColumn_1.setText("R\u00E9gime alimentaire");
 		
 		TableItem tableItem_1 = new TableItem(table, SWT.NONE);
+		tableItem_1.setText("");
 		
 		TableItem tableItem = new TableItem(table, SWT.NONE);
 		tableItem.setText(new String[] {});
@@ -101,10 +98,6 @@ public class cantine_prof {
 		Button btnTrierParNom = new Button(shlCantineprof, SWT.NONE);
 		btnTrierParNom.setBounds(67, 43, 122, 35);
 		btnTrierParNom.setText("Trier par nom");
-		
-		Button btnTrierParClasse = new Button(shlCantineprof, SWT.NONE);
-		btnTrierParClasse.setBounds(267, 43, 148, 35);
-		btnTrierParClasse.setText("Trier par classe");
 		
 		Label lblTotalDuMois = new Label(shlCantineprof, SWT.NONE);
 		lblTotalDuMois.setBounds(693, 583, 209, 28);
